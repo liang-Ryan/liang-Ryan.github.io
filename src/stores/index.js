98a1cbc2ea28cbc815e96df03f86f76a85c65b35
liang <lwy.work@outlook.com>
2024-06-08 01:28:27 +0800

    提交html文件

--- src/stores/index.js
+++
@@ -1,9 +0,0 @@
-// pinia配置
-import { createPinia } from 'pinia'
-const pinia = createPinia()
-export default pinia
-
-// pinia-plugin-persistedstate插件
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-pinia.use(piniaPluginPersistedstate)
-export * from './modules/store'
